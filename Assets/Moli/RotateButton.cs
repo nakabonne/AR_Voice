39acba9f85d7673110af24e196c2ba3ca20727c6
竹内淳市 <araiguma.227@gmail.com>
2017-06-11 01:49:03 +0900

    Add Assets

---
+++ Assets/Moli/RotateButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class RotateButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	public Transform t;
+	public int val;
+	bool on = false;
+
+	public void OnPointerDown(UnityEngine.EventSystems.PointerEventData data)
+	{
+		on = true;
+	}
+
+	public void OnPointerUp(UnityEngine.EventSystems.PointerEventData data)
+	{
+		on = false;
+	}
+
+	void Update ()
+	{
+		if (on)
+			t.Rotate (Vector3.up * val * 5);
+	}
+}
---
+++ Assets/Realistic Effects Pack/Scripts/Prefabs/Buffs/DebuffOnEnemyFromCollision.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebuffOnEnemyFromCollision : MonoBehaviour
+{
+
+  public EffectSettings EffectSettings;
+  public GameObject Effect;
+	// Use this for initialization
+	void Start () {
+    EffectSettings.CollisionEnter += EffectSettings_CollisionEnter;
+	}
+
+  void EffectSettings_CollisionEnter(object sender, CollisionInfo e)
+  {
+    if (Effect==null)
+      return;
+    var colliders = Physics.OverlapSphere(transform.position, EffectSettings.EffectRadius, EffectSettings.LayerMask);
+    foreach (var coll in colliders) {
+      var hitGO = coll.transform;
+      var renderer = hitGO.GetComponentInChildren<Renderer>();
+      var effectInstance = Instantiate(Effect) as GameObject;
+      effectInstance.transform.parent = renderer.transform;
+      effectInstance.transform.localPosition = Vector3.zero;
+      effectInstance.GetComponent<AddMaterialOnHit>().UpdateMaterial(coll.transform);
+    }
+  }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
