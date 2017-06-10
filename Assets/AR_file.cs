08db6999d172d7962b2e168a2bf507ddfa77e6ab
Kawamata Takuya <takuya@M-Kawamata-TakuyanoMacBook-Pro.local>
2017-06-10 17:23:13 +0900

    ar_test

---
+++ Assets/AR_file.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AR_file : MonoBehaviour {
+/****************************
+	AR
+	AR_testScene
+	Resources
+	SrreamingAssets
+	Vuforia
+*****************************/
+}
