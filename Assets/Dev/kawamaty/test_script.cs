7df9e5d9fcccd115076fb372bb2da9508a0ba843
Kawamata Takuya <takuya@M-Kawamata-TakuyanoMacBook-Pro.local>
2017-06-11 02:22:38 +0900

    help

--- Assets/Dev/kawamaty/test_script.cs
+++ Assets/Dev/kawamaty/test_script.cs
@@ -17,16 +17,23 @@ public class test_script : MonoBehaviour {
 		GameObject player = GameObject.Find ("Player");
 
 		if (Input.GetKeyDown (KeyCode.Z)) {
-			//GameObject bullets = GameObject.Instantiate (bullet)as GameObject;
+			/*GameObject bullets = GameObject.Instantiate (bullet)as GameObject;
+			bullet.transform.position = Vector3.MoveTowards(bullet.transform.position, player.transform.position,step);*/
+
+
+			//http://qiita.com/JunShimura/items/268d5a6ccce303999da8 の位置を指定して配置する
 			Vector3 placePosition = new Vector3(-15.6f,9.5f,0);
 			Quaternion q = new Quaternion();
 			q= Quaternion.identity;
 			Instantiate (bullet,placePosition,q);
-			//bullet.transform.position = Vector3.MoveTowards(bullet.transform.position, player.transform.position,1);
 		}
-		//bullet.transform.position = Vector3.MoveTowards(bullet.transform.position, player.transform.position,step);
-		bullet.transform.position = Vector3.MoveTowards(bullet.transform.position, player.transform.position,1);
 
+		bullet.transform.position = Vector3.MoveTowards(bullet.transform.position, player.transform.position,step); 
+		//この処理でインスタンスしたbulletもplayer方向に移動させたい
+		//いまはインスタンスできるけど、bulletがこの処理で動かない
+
+
+	//bullet.transform.position = Vector3.MoveTowards(bullet.transform.position, player.transform.position,step);
 	//		Debug.Log ("current ::  "+transform.position);
 	//		Debug.Log (player.transform.position);
 	}
