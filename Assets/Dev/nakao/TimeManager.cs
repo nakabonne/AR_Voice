e91b25bcd50cd102e789e6114f187357eecdea38
ryonakao <rodriguez.nak@gmail.com>
2017-06-10 19:24:15 +0900

    TimeManager大枠作成

--- Assets/Dev/nakao/TimeManager.cs
+++ Assets/Dev/nakao/TimeManager.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeManager : MonoBehaviour {
+public class TimeManager : SingletonMonoBehaviour<TimeManager> {
+
+	[SerializeField]
+	static float timeLimit = 60;
+	public float time = timeLimit;
+	[SerializeField]
+	bool isStarted = false;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () {        
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (isStarted) {
+			CountDown ();
+			if (time < 0.0f) {
+				time = 0.0f;
+			}
+		}
+	}
+
+	public void InitTimer(){
+		time = timeLimit;
+	}
+
+	public void StartTimer(){
+		isStarted = true;
+	}
+
+	void CountDown(){
+		if (time > 0) {
+			time -= Time.deltaTime;
+		}
+	}
+
+	public void EndCount(){
+		isStarted = false;
 	}
 }
