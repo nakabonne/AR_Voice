937e1e9cc719811b1e3dd965baa752b74589fa09
ryonakao <rodriguez.nak@gmail.com>
2017-06-10 22:05:09 +0900

    MySceneManager作成

---
+++ Assets/Scripts/Lib/MySceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : SingletonMonoBehaviour<MySceneManager> {
+
+	public static void GoToTitle(){
+		SceneManager.LoadScene ("Title");
+	}
+
+	public static void GoToStageSelect(){
+		SceneManager.LoadScene ("StageSelect");
+	}
+
+	public static void GoToMain(){
+		SceneManager.LoadScene ("Main");
+	}
+
+	public static void GoToResult(){
+		SceneManager.LoadScene ("Result");
+	}
+}
