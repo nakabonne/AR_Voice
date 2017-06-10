92b9f31c10754394ed4da24d73e6d53f33701133
ryonakao <rodriguez.nak@gmail.com>
2017-06-11 03:23:44 +0900

    マティ取得

---
+++ Assets/Dev/kawamaty/TestGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGenerator : MonoBehaviour {
+
+	public GameObject bullet;
+	float count = 0;
+	// Update is called once per frame
+	void Update () {
+		count += Time.deltaTime*1;
+		if (count >= 2) {
+			Instantiate (bullet);
+			count = 0;
+		}
+	}
+}
--- Assets/Dev/kawamaty/test_script.cs
+++ Assets/Dev/kawamaty/test_script.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class test_script : MonoBehaviour {
 	public GameObject bullet;
+	GameObject player;
 	// Use this for initialization
 	void Start () {
-		
+		player = GameObject.Find ("Player");
 	}
 	
 	// Update is called once per frame
@@ -14,7 +15,7 @@ public class test_script : MonoBehaviour {
 		float speed = 10.0f;
 		float step = Time.deltaTime * speed;
 
-		GameObject player = GameObject.Find ("Player");
+
 
 		if (Input.GetKeyDown (KeyCode.Z)) {
 			/*GameObject bullets = GameObject.Instantiate (bullet)as GameObject;
@@ -25,10 +26,10 @@ public class test_script : MonoBehaviour {
 			Vector3 placePosition = new Vector3(-15.6f,9.5f,0);
 			Quaternion q = new Quaternion();
 			q= Quaternion.identity;
-			Instantiate (bullet,placePosition,q);
+			//Instantiate (bullet,placePosition,q);
 		}
 
-		bullet.transform.position = Vector3.MoveTowards(bullet.transform.position, player.transform.position,step); 
+		this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position,step); 
 		//この処理でインスタンスしたbulletもplayer方向に移動させたい
 		//いまはインスタンスできるけど、bulletがこの処理で動かない
 
