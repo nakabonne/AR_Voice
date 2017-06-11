721cf16977cc1277486d29746de702927f69538c
Kawamata Takuya <takuya@M-Kawamata-TakuyanoMacBook-Pro.local>
2017-06-11 12:41:32 +0900

    title,StageSelect,ResultのUI BGM,SE追加

--- Assets/Scripts/Result/RestartButton.cs
+++ Assets/Scripts/Result/RestartButton.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class RestartButton : MonoBehaviour {
 
+	void Start() {
+		AudioManager.Instance.PlayBGM("bgm_maoudamashii_cyber33");
+	}
+
 	public void Restart(){
 		ScoreManager.Instance.Reset ();
 		MySceneManager.Instance.GoToStageSelect ();
--- Assets/Scripts/Title/StartButton.cs
+++ Assets/Scripts/Title/StartButton.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StartButton : MonoBehaviour {
+	void Start() {
+		AudioManager.Instance.PlayBGM("bgm_maoudamashii_cyber40");
+	}
 
 	public void ButtonStart(){
 		MySceneManager.Instance.GoToStageSelect();
+
 	}
 }
